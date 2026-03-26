cf8decd85c9048e365289dbd44dada81017ea9dd
Michael Huang <michaelhly@gmail.com>
2026-03-25 23:14:51 -0400

    format

--- packages/selenium-impl/src/driver/index.ts
+++ packages/selenium-impl/src/driver/index.ts
@@ -2,18 +2,16 @@ import { createDriver, type Driver } from "@michaelhly.webdriver-c11y/schemas";
 import { createContext } from "../components/context.js";
 import { buildBidiComponents } from "./bidi.js";
 import {
-	type SeleniumDriverOptions,
 	applyBrowserOptions,
 	buildClassicComponents,
+	type SeleniumDriverOptions,
 } from "./classic.js";
 
-export type { SeleniumDriverOptions } from "./classic.js";
 export { createSeleniumBidiDriver } from "./bidi.js";
+export type { SeleniumDriverOptions } from "./classic.js";
 export { createSeleniumClassicDriver } from "./classic.js";
 
-export function createSeleniumDriver(
-	options?: SeleniumDriverOptions,
-): Driver {
+export function createSeleniumDriver(options?: SeleniumDriverOptions): Driver {
 	const ctx = createContext();
 	if (options) applyBrowserOptions(ctx, options);
 	return createDriver({
--- packages/selenium-impl/src/index.ts
+++ packages/selenium-impl/src/index.ts
@@ -1,10 +1,10 @@
-export { ChromeOptionsBuilder } from "./options/chrome.js";
-export { EdgeOptionsBuilder } from "./options/edge.js";
-export { FirefoxOptionsBuilder } from "./options/firefox.js";
-export { SafariOptionsBuilder } from "./options/safari.js";
 export type { SeleniumDriverOptions } from "./driver/index.js";
 export {
 	createSeleniumBidiDriver,
 	createSeleniumClassicDriver,
 	createSeleniumDriver,
 } from "./driver/index.js";
+export { ChromeOptionsBuilder } from "./options/chrome.js";
+export { EdgeOptionsBuilder } from "./options/edge.js";
+export { FirefoxOptionsBuilder } from "./options/firefox.js";
+export { SafariOptionsBuilder } from "./options/safari.js";
