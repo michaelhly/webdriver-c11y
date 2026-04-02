6c40c2154ebd0aa256392d82f3db7c368358c41e
Michael Huang <michaelhly@gmail.com>
2026-04-01 20:14:38 -0400

    Update

---
+++ packages/selenium-impl/tests/chrome/capabilities.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from "vitest";
+
+import { ChromeOptions, createSeleniumDriver } from "../../src/index.js";
+
+test("Basic Capabilities", async () => {
+  const driver = createSeleniumDriver();
+  const session = await driver.newSession({
+    capabilities: { alwaysMatch: { browserName: "chrome" } },
+  });
+  expect(session.capabilities.browserName).toBe("chrome");
+
+  await driver.navigateTo({ url: "https://selenium.dev" });
+
+  const { title } = await driver.getTitle();
+  expect(title).toBe("Selenium");
+
+  await driver.deleteSession();
+});
+
+describe("Capabilities with selenium options", async () => {
+  test("headless", async () => {
+    const options = new ChromeOptions();
+    options.addArguments("--headless");
+    const driver = createSeleniumDriver({ chrome: options });
+    await driver.newSession({
+      capabilities: { alwaysMatch: { browserName: "chrome" } },
+    });
+
+    const { value: userAgent } = await driver.executeScript({
+      script: "return navigator.userAgent;",
+    });
+    expect(userAgent).toContain("HeadlessChrome");
+  });
+});
+
